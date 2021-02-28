5b1e9ff9bc5d1576d54295345a4f5fcf6af7dd21
SanjanaSrabanti16 <ssraba2@uic.edu>
2021-02-27 19:05:48 -0600

    Output change

--- infogain.py
+++ infogain.py
@@ -10,12 +10,13 @@ fileInputList = list()
 attributeList = list()
 classList = list()
 entropyList = list()
+infoGainList = list()
 classDict = dict()
 infoGain = 0
 datasetEntropy = 0
 
 def readFile():
-	with open(os.path.join(__location__, 'golf.txt')) as dataFile:
+	with open(os.path.join(__location__, 'Sampledata3.txt')) as dataFile:
 		for line in dataFile:
 			fileInputList.append(list())
 			fileInputList[len(fileInputList) - 1] = line.replace('\n', '').replace(' ', '').lower().split(',')
@@ -27,14 +28,17 @@ readFile()
 for line in fileInputList:
     classList.append(line[len(line) - 1])
     line.pop(len(line) - 1)
+    
 
 for attr in fileInputList[0]:
     attributeList.append(list())
+    
 
 for line in fileInputList:
     for index, attr in enumerate(line):
         attributeList[index].append(attr)
-
+        
+#print(attributeList)
 classCounter = Counter(classList)
 
 for className in np.unique(classList):
@@ -44,7 +48,7 @@ for className in np.unique(classList):
 for className in np.unique(classList):
     datasetEntropy += classCounter[className]/len(fileInputList) * np.log2(classCounter[className]/len(fileInputList))
 datasetEntropy = -datasetEntropy
-print(datasetEntropy)
+#print(datasetEntropy)
 
 
 for column in attributeList:
@@ -62,10 +66,22 @@ for column in attributeList:
             classDict[className] = 0
     entropyList.append(columnEntropyValue)
 
-print(entropyList)
+#print(entropyList[0])
+for i in entropyList:
+    infoGain = datasetEntropy - i
+    infoGainList.append(infoGain)
+
+infoGainList.sort(reverse=True)
+print(infoGainList)
+
+
+with open('result3.txt', 'w') as output:
+    output.write("(IG {})".format(infoGainList[0]))
+output.close()
+
 
 if(len(sys.argv) > 1 and int(sys.argv[1]) < len(entropyList)):
     infoGain = datasetEntropy - entropyList[int(sys.argv[1])]
-    with open('result.txt', 'w') as output:
+    with open('result3.txt', 'w') as output:
         output.write("(IG {})".format(infoGain))
     output.close()
