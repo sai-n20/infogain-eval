0de56b9b8f46ecefe1708d435bec606bf026e046
Sai Nadkarni <sai2010nadkarni@gmail.com>
2021-03-07 19:07:05 -0600

    add comments and misc fixes

--- infogain.py
+++ infogain.py
@@ -16,7 +16,7 @@ infoGain = 0
 datasetEntropy = 0
 
 def readFile():
-	with open(os.path.join(__location__, 'Sampledata3.txt')) as dataFile:
+	with open(os.path.join(__location__, 'data.txt')) as dataFile:
 		for line in dataFile:
 			fileInputList.append(list())
 			fileInputList[len(fileInputList) - 1] = line.replace('\n', '').replace(' ', '').lower().split(',')
@@ -25,37 +25,40 @@ def readFile():
 
 readFile()
 
+#Get list of classes
 for line in fileInputList:
     classList.append(line[len(line) - 1])
     line.pop(len(line) - 1)
     
-
+#Prepare attribute list
 for attr in fileInputList[0]:
     attributeList.append(list())
     
-
+#Add attributes to corresponding lists
 for line in fileInputList:
     for index, attr in enumerate(line):
         attributeList[index].append(attr)
-        
-#print(attributeList)
+
+#Create Counter object for counting class instances
 classCounter = Counter(classList)
 
+#Initialize class dictionary to be used during entropy calculation
 for className in np.unique(classList):
     classDict[className] = 0
 
-
+#Calculate raw dataset entropy
 for className in np.unique(classList):
     datasetEntropy += classCounter[className]/len(fileInputList) * np.log2(classCounter[className]/len(fileInputList))
 datasetEntropy = -datasetEntropy
-#print(datasetEntropy)
 
-
+#Calculate entropy per column
 for column in attributeList:
     columnEntropyValue = 0
     for attr in np.unique(column):
         attrEntropyValue = 0
         indexes = [i for i, e in enumerate(column) if e == attr]
+        
+        #Count instances of attribute contributing to count of particular class
         for i in indexes:
             classDict[classList[i]] += 1
         for className in np.unique(classList):
@@ -66,7 +69,6 @@ for column in attributeList:
             classDict[className] = 0
     entropyList.append(columnEntropyValue)
 
-#print(entropyList[0])
 for i in entropyList:
     infoGain = datasetEntropy - i
     infoGainList.append(infoGain)
@@ -75,13 +77,13 @@ infoGainList.sort(reverse=True)
 print(infoGainList)
 
 
-with open('result3.txt', 'w') as output:
+with open('result.txt', 'w') as output:
     output.write("(IG {})".format(infoGainList[0]))
 output.close()
 
 
 if(len(sys.argv) > 1 and int(sys.argv[1]) < len(entropyList)):
     infoGain = datasetEntropy - entropyList[int(sys.argv[1])]
-    with open('result3.txt', 'w') as output:
+    with open('result.txt', 'w') as output:
         output.write("(IG {})".format(infoGain))
     output.close()
